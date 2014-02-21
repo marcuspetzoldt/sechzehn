c4312f05ac0486233a4f3f4baeade3cfd51b6f1c
Marcus Petzoldt <marcus.petzoldt@gmail.com>
2014-02-21 18:59:57 +0100

    better Keywords and Headers (SEO)

--- app/helpers/session_helper.rb
+++ app/helpers/session_helper.rb
@@ -2,14 +2,14 @@ module SessionHelper
 
   def sign_in(user)
     remember_token = User.new_remember_token
-    cookies.permanent[:remember_token] = { value: remember_token, domain: :all }
+    cookies.permanent[:remember_token] = { value: remember_token, domain: 'sechzehn.org' }
     user.update_attribute(:remember_token, User.encrypt(remember_token))
     self.current_user = user
   end
 
   def sign_out
     current_user.update_attribute(:remember_token, User.encrypt(User.new_remember_token))
-    cookies.delete(:remember_token, domain: :all)
+    cookies.delete(:remember_token, domain: 'sechzehn.org')
     self.current_user = nil
   end
 
