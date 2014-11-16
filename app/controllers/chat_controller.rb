040a264dbf54b218012e018015940f010936d0f2
Marcus Petzoldt <marcus.petzoldt@gmail.com>
2014-11-16 20:18:53 +0100

    add chat

---
+++ app/controllers/chat_controller.rb
@@ -0,0 +1,19 @@
+class ChatController < ApplicationController
+  def new
+  end
+
+  def save
+    c = Chat.new(user_id: current_user.id, chat: params[:chat][:chat])
+    c.save
+    @chats = Chat.where("created_at > '#{Time.now.utc - 30.minutes}'").order(:created_at)
+    redirect_to chat_show_path
+  end
+
+  def messages
+    @chats = Chat.where("created_at > '#{Time.now.utc - 30.minutes}'").order(:created_at)
+  end
+
+  def show
+    @chats = Chat.where("created_at > '#{Time.now.utc - 30.minutes}'").order(:created_at)
+  end
+end
