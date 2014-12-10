c7afd53ae27ba485d62c1b971ce068a259e8b8e9
Marcus Petzoldt <marcus.petzoldt@gmail.com>
2014-12-10 20:14:07 +0100

    Modified Chat model and controller in accordance to inspection suggestions

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -1,22 +1,20 @@
 class ChatController < ApplicationController
 
   def save
-    c = Chat.new(user_id: current_user.id, chat: params[:chat][:chat])
-    c.save
-    @chats = Chat.where("created_at > '#{Time.now.utc - 30.minutes}'").order(:created_at)
+    @chats = Chat.send_message(current_user.id, params[:chat][:chat])
     redirect_to chat_show_path
   end
 
   def messages
-    @chat = nil
+    @chats = nil
     max_id = Chat.maximum(:id)
     if session['most_recent_chat'].nil? or session['most_recent_chat'] != max_id
       session['most_recent_chat'] = max_id
-      @chats = Chat.where("created_at > '#{Time.now.utc - 30.minutes}'").order(:created_at)
+      @chats = Chat.get_newest_messages
     end
   end
 
   def show
-    @chats = Chat.where("created_at > '#{Time.now.utc - 30.minutes}'").order(:created_at)
+    @chats = Chat.get_newest_messages
   end
 end
--- app/models/chat.rb
+++ app/models/chat.rb
@@ -2,4 +2,13 @@ class Chat < ActiveRecord::Base
 
   belongs_to :user
 
+  def self.send_message(user_id, message)
+    Chat.create(user_id: user_id, chat: message)
+    self.get_newest_messages
+  end
+
+  def self.get_newest_messages
+    Chat.where("created_at > '#{Time.now.utc - 30.minutes}'").order(:created_at)
+  end
+
 end
