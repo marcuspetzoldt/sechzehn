d60e9cbdef34a232918c78ba0dd3297ea3513c0e
Marcus Petzoldt <marcus.petzoldt@gmail.com>
2015-03-29 14:58:25 +0200

    fixed suppress blank lines in chat

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,14 +1,15 @@
 class ChatsController < ApplicationController
 
   def save
-    ip = request.remote_ip
-    Rails.logger.info("IP-Address: #{ip}")
-    unless Ban.find_by(ip: ip)
-      Chat.create(user_id: current_user.id, chat: params[:chat][:chat], ip: ip)
-      Pusher['sechzehn'].trigger('chats', {
-        user: User.find(current_user.id).name,
-        message: params[:chat][:chat]
-      })
+    unless params[:chat][:chat].blank?
+      ip = request.remote_ip
+      unless Ban.find_by(ip: ip)
+        Chat.create(user_id: current_user.id, chat: params[:chat][:chat], ip: ip)
+        Pusher['sechzehn'].trigger('chats', {
+          user: User.find(current_user.id).name,
+          message: params[:chat][:chat]
+        })
+      end
     end
     render nothing: true
   end
