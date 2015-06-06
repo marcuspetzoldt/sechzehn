6f799feea03908ff9aaf78bbff84d38cf0549a09
Marcus Petzoldt <marcus.petzoldt@gmail.com>
2015-06-06 11:10:05 +0200

    changed sending messages via firebase from set to push

--- app/helpers/chat_helper.rb
+++ app/helpers/chat_helper.rb
@@ -1,6 +1,6 @@
 module ChatHelper
   def firebase_say(name, message, system)
     firebase = Firebase::Client.new('https://luminous-inferno-1701.firebaseio.com/', '5mGdZZ5NoMqtEam3KwY8ZfB5QXOeG0RfvgAf3NK2')
-    firebase.set('chat', { usr: name, msg: message, sys: system })
+    firebase.push('chat', { usr: name, msg: message, sys: system })
   end
 end
