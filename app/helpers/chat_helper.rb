e9e33ba743defdf2758ab894d4000d63d0c7397e
Marcus Petzoldt <marcus.petzoldt@gmail.com>
2015-06-21 15:11:57 +0200

    added total words and total points to solution view

--- app/helpers/chat_helper.rb
+++ app/helpers/chat_helper.rb
@@ -1,6 +1,6 @@
 module ChatHelper
   def firebase_say(name, message, system)
     firebase = Firebase::Client.new('https://luminous-inferno-1701.firebaseio.com/', '5mGdZZ5NoMqtEam3KwY8ZfB5QXOeG0RfvgAf3NK2')
-    firebase.push('chat', { usr: name, msg: message, sys: system })
+    firebase.set('chat', { usr: name, msg: message, sys: system })
   end
 end
