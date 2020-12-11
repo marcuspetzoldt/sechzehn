b1535a6c40074cc581ac2179ff617de100117605
Marcus <marcus.petzoldt@gmail.com>
2020-12-11 16:47:38 +0100

    Configuration for SMTP Mail in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,19 @@ Sechzehn::Application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.strato.de',
+    port:                 465,
+    domain:               'sechzehn.org',
+    user_name:            Rails.application.credentials.mail[:user],
+    password:             Rails.application.credentials.mail[:pass],
+    authentication:       'login',
+    ssl:                  true,
+    enable_starttls_auto: true,
+    openssl_verify_mode:  'none'
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
